2b2f2bff3ab520543857287419e784de11683b7d
ivan <ivanfan@connect.hku.hk>
2025-03-30 16:54:56 +0800

    修复bug

--- main.py
+++ main.py
@@ -15,15 +15,6 @@ if __name__ == "__main__":
     chat_room.add_charactor(charactor('Emily', [0.6, 0.3, 0.2, 0.8], chat_room, background="你是一个时尚女大,爱逛街,买衣服,打扮,但是你暗恋你的闺蜜Helen,对男生置之不理"))  # ESTJ
     chat_room.add_charactor(charactor('George', [0.2, 0.4, 0.3, 0.3], chat_room, background="你是一个大学校草,你与David是好兄弟,你为高冷,不爱凑热闹")) # ISTP
     chat_room.add_charactor(charactor('Helen', [0.3, 0.2, 0.7, 0.6], chat_room, background="你是一个自卑的女大学生,Emily是你的好闺蜜,常常倾听她的诉求"))  # ISFJ
-    chat_room = ChatRoom()
-    chat_room.add_charactor(charactor('David', [0.7, 0.6, 0.8, 0.7], chat_room, background="你是香港大学社会学系的学生，是ENFJ,喜欢组织社交活动和听流行音乐。"
-        "你和Emily是同班同学，擅长激发团队灵感；和Helen是学生会同事，欣赏她的细心。"))  # ENFJ
-    chat_room.add_charactor(charactor('Emily', [0.6, 0.3, 0.2, 0.8], chat_room, background="你是香港大学社会学系的学生，是ESTJ,喜欢制定计划和喝港式奶茶。"
-        "你和David是同班同学，负责落实想法；你是George的堂姐，常叮嘱他。"))  # ESTJ
-    chat_room.add_charactor(charactor('George', [0.2, 0.4, 0.3, 0.3], chat_room, background="你是香港大学工程系的学生，是ISTJ,喜欢玩滑板和修电子产品。"
-        "你是Emily的堂弟，随性不爱被管；和Helen是社团朋友，帮她修东西。")) # ISTP
-    chat_room.add_charactor(charactor('Helen', [0.3, 0.2, 0.7, 0.6], chat_room, background="你是香港大学心理学系的学生，是ISFJ,喜欢做手工艺和吃港式甜品。"
-        "你和George是社团朋友，欣赏他独立；和David是学生会同事，视他为领袖。"))  # ISFJ
     # chat_room.add_charactor(charactor('Ivy', [0.4, 0.8, 0.9, 0.4], chat_room))    # INFP
     # chat_room.add_charactor(charactor('Jack', [0.8, 0.4, 0.7, 0.3], chat_room))   # ESFP
     # chat_room.add_charactor(charactor('Kelly', [0.9, 0.7, 0.3, 0.2], chat_room))  # ENTP
